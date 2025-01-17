b45a976c60edc44d924fe58f46366b8da478af99
ZeroWillHero <ict21018@std.uwu.ac.lk>
2025-01-17 12:14:43 +0530

    Front end Registration V1 created

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,32 @@
-const submitData = () => {
-    
-}
+const submitData = async (e) => {
+    e.preventDefault();
+    const data = {
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        studentId: document.getElementById('studentId').value,
+        password: document.getElementById('password').value,
+        phoneNumber: document.getElementById('phone').value,
+        address: document.getElementById('address').value,
+    };
+
+    try {
+        const response = await fetch('http://localhost:3000/api/students/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const result = await response.json();
+        console.log('Success:', result);
+        window.location.href = 'Success.html';
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
